b7b64d38e069bbfd2de7c1b63ef853769d7dbd11
chiekhou <chiekhou.traore@gmail.com>
2026-02-04 14:28:29 +0100

    decommenté le service email

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -20,26 +20,25 @@ async function sendEmail(to, subject, html) {
   console.log("==============================================\n");
 
   // TODO: Implémenter l'envoi réel d'email avec nodemailer
-  /*
-  const nodemailer = require('nodemailer');
-  
+
+  const nodemailer = require("nodemailer");
+
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
     secure: true,
     auth: {
       user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS
-    }
+      pass: process.env.SMTP_PASS,
+    },
   });
 
   await transporter.sendMail({
     from: '"Sakinah" <noreply@sakinah.app>',
     to: to,
     subject: subject,
-    html: html
+    html: html,
   });
-  */
 
   return true;
 }
